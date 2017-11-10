da2ff77592a58f068294902cc0b3c6ef37aa8cb4
Puneeth Chaganti <punchagan@muse-amuse.in>
2017-11-10 11:00:34 +0530

    Add code to walk over all the commits of a repo

--- src/preview/repository.clj
+++ src/preview/repository.clj
@@ -3,6 +3,7 @@
   (:require [preview.config :refer [repository-root]]
             [clojure.java.io :as io]
             [clj-jgit.porcelain :as git]
+            [clj-jgit.querying :as gq]
             [clojure.string :as str]))
 
 (defmacro with-repo [repo-name & body]
@@ -23,3 +24,16 @@
   (with-repo repo-name
     (git/git-checkout repo branch)
     (repo-state repo-name)))
+
+(defn walk-repo-commits
+  "Walk over all commits in the repo, and execute the callable."
+  [repo-name callable & args]
+  (with-repo repo-name
+    (defn- walk-fn [sha]
+      (git/git-checkout repo sha)
+      (apply callable repo-name sha args))
+    (let [commit-shas (map branch-name (gq/rev-list repo))
+          current (git/git-branch-current repo)
+          v (doall (map walk-fn commit-shas))]
+      (git/git-checkout repo current)
+      v)))
